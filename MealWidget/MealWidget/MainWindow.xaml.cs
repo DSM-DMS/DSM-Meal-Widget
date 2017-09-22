0968abd27aa81a6ec87e6146a62ba6cd826fa22a
이종현 <dwg7691@gmail.com>
2017-09-22 13:17:17 +0900

    Checkout Branch

--- MealWidget/MealWidget/MainWindow.xaml.cs
+++ MealWidget/MealWidget/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace MealWidget
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
         static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
         const UInt32 SWP_NOSIZE = 0x0001;
         const UInt32 SWP_NOMOVE = 0x0002;
         const UInt32 SWP_NOACTIVATE = 0x0010;
